0b6b27cf064b907eb78222b4bb3ec9b0a9e18563
sailalith <lalithvoop@hotmail.com>
2024-04-22 07:23:14 +0530

    adding a dummy policy

--- module/main.tf
+++ module/main.tf
@@ -59,7 +59,8 @@ resource "aws_iam_role" "role" {
       Version = "2012-10-17"
       Statement = [
         {
-          Action   = var.policy_resource_list
+          # Action   = var.policy_resource_list
+          Action = concat(var.dummy_policy,var.policy_resource_list)
           Effect   = "Allow"
           Resource = "*"
         },
